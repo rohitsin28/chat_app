d61a57f5112510dca360331aa93d047296c703a7
rohitsingh <rohitsin28@gmail.com>
2025-03-29 19:18:40 +0530

    create vite frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <div className='text-red-500 text-center'>
+        Hello Rohit Singh
+      </div>
+    </>
+  )
+}
+
+export default App
