3c8b93fa2740c4af2be4498fa4c726470e5cae48
rohitsingh <rohitsin28@gmail.com>
2025-03-29 17:37:39 +0530

    create react app and setup nodejs

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,20 @@
 import express from "express";
+import chats from "./data/data.js";
+import dotenv from "dotenv";
 
+dotenv.config({ path: "../.env" });
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.listen(3001,()=>{
-    console.log(`Server starton http://localhost:3001`)
-})
+app.get("/", (req, res) => {
+  return res.send("Hello Rohit Singh");
+});
+
+app.get("/api/chat", (req, res) => {
+  return res.send(chats);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
